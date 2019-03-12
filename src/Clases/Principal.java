205ddd9612b4854385bb769d0919985d9b459d3b
SantiagoRP7 <rodriguez.santiago@correounivalle.edu.co>
2019-03-11 21:26:40 -0500

    restaura inicio via login

--- src/Clases/Principal.java
+++ src/Clases/Principal.java
@@ -44,8 +44,8 @@ public class Principal extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		escenarioLogin = primaryStage;
-		//iniciarLogin();
-		iniciarGerente("Ximena Guzman");
+		iniciarLogin();
+		//iniciarGerente("Ximena Guzman");
 	}
 
 	public static void main(String[] args) {

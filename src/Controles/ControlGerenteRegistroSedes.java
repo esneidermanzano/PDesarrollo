4d03eae28ddca7ae887c9963a566b9d93dac2a3d
esneidermanzano <esneidermanzano@gmail.com>
2019-04-23 19:59:22 -0500

    Version 4.6

--- src/Controles/ControlGerenteRegistroSedes.java
+++ src/Controles/ControlGerenteRegistroSedes.java
@@ -7,16 +7,16 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 
 import BaseDatos.DaoSede;
-import javafx.animation.FadeTransition;
+import Clases.Validador;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.text.Text;
 import javafx.scene.control.Alert.AlertType;
-import javafx.util.Duration;
 public class ControlGerenteRegistroSedes {
 	private DaoSede sede;
+	private Validador validador;
 	
     @FXML private JFXTextField campoNombre;
     @FXML private JFXTextField campoTelefono;
@@ -36,6 +36,7 @@ public class ControlGerenteRegistroSedes {
     
     public void initialize() {    	
     	sede = new DaoSede();
+    	validador = new Validador();
     	campoNombre.setOnKeyPressed((e) -> {
 			errorNombre.setText("");
 		});
@@ -65,12 +66,20 @@ public class ControlGerenteRegistroSedes {
     	LocalDate localDate = fechaRegistroSedes.getValue();
     	String fecha = "";
 		int valido = 0;
+		
 		if(nombre.replace(" ", "").equals("")) {
 			errorNombre.setText(("El campo Nombre esta vacio"));
    		 	return false;
 		}
+		nombre = validador.ajustar(nombre);
+		campoNombre.setText(nombre);
+		if(!nombre.replace(" ", "").matches("[A-Za-z0-9 ]*")) {
+			errorNombre.setText(("No se aceptan caracteres especiales"));
+			return false;
+		}
+		
 		if(nombre.length()>50) {
-			errorNombre.setText(("El nombre es muy largo"));
+			errorNombre.setText(("Maximo 50 caracteres"));
 			return false;
 		}		
 		
@@ -78,44 +87,77 @@ public class ControlGerenteRegistroSedes {
     		errorTelefono.setText(("El campo Telefono esta vacio"));
        		 return false;
     	}else {
+    		telefono = validador.ajustar(telefono);
+    		campoTelefono.setText(telefono);
     		if(telefono.matches("[0-9]*")) {
     			if(telefono.length()<6 || telefono.length()>10) {
     				errorTelefono.setText(("El Telefono tener entre 6 y 10 números"));
         			return false;
     			}
     		}else {
-    			errorTelefono.setText(("El Telefono debe ser numeros"));    			
+    			errorTelefono.setText(("El telefonosolo son números"));    			
     			return false;
     			}
     	}
+    	    	
     	if(direccion.replace(" ", "").equals("")) {
     		errorDireccion.setText(("El campo Direccion esta vacio"));
    		 	return false;
     	} 
-    	if(direccion.length()>50) {
-    		errorDireccion.setText(("La direccion es muy larga"));
+    	direccion = validador.ajustar(direccion);
+    	campoDireccion.setText(direccion);
+    	
+    	if(direccion.matches(".*[+$%&'?!=¡¿/].*")) {
+    		errorDireccion.setText(("Caracter no aceptado"));
 			return false;
-		}	
+    	}
+    	if(direccion.length()<5 || direccion.length()>50) {
+    		errorDireccion.setText(("La direccion es entre 5 y 50 caracteres"));
+			return false;
+		}
+    	
     	if(tamano.equals("")) {
     		errorTamano.setText(("El campo Tamaño esta vacio"));
    		 	return false;
     	}else {
-    		if(tamano.matches("[0-9]*")) {	    		
+    		tamano = validador.ajustar(tamano);
+    		campoTamano.setText(tamano);
+    		if(tamano.matches("[0-9]*")) {
+    			if(tamano.length()>10) {
+    				errorTamano.setText(("El tamaño no debe superar 10 números"));
+        			return false;
+    			}
+    			if(tamano.matches("[0]*")) {
+    				errorTamano.setText(("El tamaño no debe ser de cero"));
+        			return false;
+    			}
     		}else {
     			errorTamano.setText(("El campo tamaño debe ser numerico"));
        		 	return false;
     		}
     	}
+    	
     	if(empleados.replace(" ", "").equals("")) {
     		errorEmpleados.setText(("El campo Empleados esta vacio"));
    		 	return false;
     	}else {
-    		if(empleados.matches("[0-9]*")) { 		    		
+    		empleados = validador.ajustar(empleados);
+    		campoEmpleados.setText(empleados);
+    		if(empleados.matches("[0-9]*")) {
+    			if(empleados.length()>6) {
+    				errorEmpleados.setText(("Los empleados superan el orden de 6"));
+        			return false;
+    			}
+    			if(empleados.matches("[0]*")) {
+    				errorEmpleados.setText(("Los empleados no deben ser cero"));
+        			return false;
+    			}
     		}else {
     			errorEmpleados.setText(("El campo Empleados debe ser solo numeros"));
        		 	return false;
     		}
     	}
+    	
     	if(localDate == null) {
     		errorFecha.setText(("El campo Fecha esta vacio"));
    		 	return false;
